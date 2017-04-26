1e1d0889f453d110bc6ac18f603bfd9ba1df74cf
Kevin Altman <kevin@itsthatguy.com>
2017-04-26 16:16:33 -0400

    cleanup

--- src/store.js
+++ src/store.js
@@ -1,8 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import reducers from './reducers/index';
 
-// Add the reducer to your store on the `router` key
-// Also apply our middleware for navigating
 const store = createStore(
   combineReducers({
     ...reducers,
